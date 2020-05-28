201761a22360ceae22fac53f83727cd934a05b88
Pedrael <mihail_krivic@mail.ru>
2020-05-28 23:04:19 +0300

    added carousel and js integration with elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,62 @@
-import jQuery from 'jquery';
-import Header from './header.js';
-import Carousel from './carousel.js';
-//import MyTimeIsNow from './date.js';
-//import Mail from './mail.js';
-import Ripple from './ripple.js';
-import Parallax from './parallax.js';
-//require("./scrollvideo.js");
+import 'owl.carousel';
 
-Header();
-Carousel();
-//Mail();
-//MyTimeIsNow("date-input");
-Ripple();
-Parallax();
+$(document).ready(() => {
+  ////////////////CAROUSEL/////////////////////////////////////////////////
+  var slider = $('#main-slider')
+  slider.owlCarousel({
+    loop: false,
+    rewind: true,
+    items: 1,
+    dots: false
+
+    //dotsContainer: '#photos-dots'
+  })
+  slider.on('changed.owl.carousel', (event) => {
+    $(".js-list-item").removeClass("active")
+    $("#item"+event.item.index).addClass("active")
+    $(".progress-bar").find(".complete").css('width', 25*(event.item.index + 1)+'%')
+  })
+  $('.js-list-item').click((event) => {
+    var id = event.currentTarget.id
+    slider.trigger('to.owl.carousel', [id.replace('item', '')])
+  })
+  $('.to-last').click((event) => {
+    slider.trigger('to.owl.carousel', [3])
+  })
+  $('.control_next').click(function() {
+      slider.trigger('next.owl.carousel')
+  })
+  $('.control_prev').click(function() {
+      slider.trigger('prev.owl.carousel')
+  })
+  ////////////////SIDEBAR TOGGLE///////////////////////////////////////////
+  $('.js-toggle-menu').click(() => {
+    var element = document.querySelector(".sidebar")
+    if(element.classList.contains('sidebar-hide')) {
+      $('.sidebar').addClass('sidebar-show')
+      $('.sidebar').removeClass('sidebar-hide')
+    }
+    else {
+      $('.sidebar').removeClass('sidebar-show')
+      $('.sidebar').addClass('sidebar-hide')
+    }
+  })
+  /////////HORIZONTAL CHECK/////////////////////////////////////////////////
+  function checkHorizontal() {
+    var hght = document.documentElement.clientHeight
+    var wdth = document.documentElement.clientWidth
+    if(wdth > hght) return true;
+    return false;
+  }
+  function showIfHorizontal() {
+    if(checkHorizontal()) {
+      $('.warning').css('display', 'none')
+    }
+    else {
+      $('.warning').css('display', 'flex')
+    }
+  }
+  showIfHorizontal()
+  window.addEventListener("resize", showIfHorizontal, {passive: true});
+  window.addEventListener("orientationchange", showIfHorizontal, {passive: true});
+})
